843e6b3dd5c8a882ea76d6b774b543dafaea765e
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-06-21 00:43:44 -0400

    Negatives and future precomputation
    Closes #4

--- main.js
+++ main.js
@@ -1,25 +1,66 @@
 (function(){'use strict';
-	const Int = BigInt
-	let lim = (1n << 68n) + 1n //already-checked ints +1
-	const check = () => {
-		let n = lim
-		do {
-			n *= 3n; n++
-			do n >>= 1n; while (!(n & 1n)) //remove all trailing zeros
-		} while (n > lim)
-		return n == lim
+	const
+		Float = Number, Int = BigInt,
+		k = 5n,
+		C = [0n], D = [0n],
+		f = n => (n & 1n ? 3n * n + 1n : n) >> 1n,
+		//remove all trailing zeros
+		trim = n => {while (!(n & 1n)) n >>= 1n; return n}
+
+	//precompute arrays
+	for (let b = 1n; b < (1n << k); b++) {
+		C[b] = b & 1n
+		C[b] += (D[b] = f(b)) & 1n
+		for (let i = 1; i < k; i++)
+			C[b] += (D[b] = f(D[b])) & 1n
 	}
+	console.table(C) //C is wrong, not D
+
+	let limPos = (1n << 68n) | 1n //already-checked positives +1
+	let limNeg = (-1n << 16n) | 1n //tested negatives
 
 	globalThis.Collatz = {
-		search: n => { //find a counterexample, if it exists
-			n = Int(n)
-			for (let i = 0n; i < n; i++, lim += 2n)
-				if (check()) return lim
-			return undefined
+		search: len => { //find a counterexample, if it exists
+			len = Float(len)
+			if (len < 0)
+				for (let i = 0, n; i > len; i--, limNeg -= 2n) {
+					n = limNeg
+					do n = trim(f(n)); while (n < limNeg)
+					if (n == limNeg) return limNeg //detect any cycle
+				}
+			else
+				for (let i = 0, n; i < len; i++, limPos += 2n) {
+					n = limPos
+					do n = trim(f(n)); while (n > limPos)
+					if (n == limPos) return limPos //detect any cycle
+				}
+		},
+		test: n => {
+			n = trim(Int(n))
+			if (n < 0n) {
+				if (n >= limNeg) return false
+				do n = trim(f(n)); while (n < limNeg)
+			}
+			else {
+				if (n <= limPos) return false
+				do n = trim(f(n)); while (n > limPos)
+			}
 		}
 	}
-	Object.defineProperty(Collatz, 'limit', {get: () => lim,
+	Object.defineProperty(Collatz, 'limitPos', {get: () => limPos,
 		//ensure it's always odd, and greater than the trivial cycle
-		set: x => {if ( !((x = Int(x)) & 1n) ) x--; if (x < 5n) x = 5n; lim = x}
+		set: x => {
+			if ( !((x = Int(x)) & 1n) ) x--
+			if (x < 5n) x = 5n
+			limPos = x
+		}
+	})
+	Object.defineProperty(Collatz, 'limitNeg', {get: () => limNeg,
+		//ensure it's always odd, and greater than the trivial cycle
+		set: x => {
+			if ( !((x = Int(x)) & 1n) ) x++
+			if (x > -273n) x = -273n
+			limNeg = x
+		}
 	})
 })()
