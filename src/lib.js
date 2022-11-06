8a58b89dab460130d5cff0b0771d05a1a8c7614f
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-11-06 01:02:25 -0400

    optimize and docs

--- src/lib.js
+++ src/lib.js
@@ -4,12 +4,16 @@ const Collatz = (() => {
 	/**
 	remove all binary trailing zeros
 	@param {bigint} n
-	*/const trim = n => { if (n) while (!(n & 1n)) n >>= 1n; return n }
+	*/
+	const trim = n => { if (n) while (!(n & 1n)) n >>= 1n; return n }
 
-	/**@param {bigint} n*/
-	const f = n => trim((n & 1n ? 3n * n + 1n : n) >> 1n)
+	/**
+	Collatz fn with "extreme shortcut" (removes all TZs)
+	@param {bigint} n
+	*/
+	const f = n => trim(n & 1n ? 3n * n + 1n : n)
 
-	/*precompute arrays
+	/*// precompute arrays
 	const k = 5n, C = [0n], D = [0n]
 	for (let b = 1n; b < (1n << k); b++) {
 		C[b] = b & 1n
@@ -17,29 +21,46 @@ const Collatz = (() => {
 		for (let i = 1; i < k; i++)
 			C[b] += (D[b] = f(D[b])) & 1n
 	}
-	console.table(C) //C is wrong, not D
+	console.table(C) // C is wrong, not D
 	*/
 
+	/** max known inclusive bound */
 	let limPos = (1n << 68n) | 1n
+	/** min known inclusive bound */
 	let limNeg = (-1n << 33n) | 1n
 
-	const Collatz = {
-		search: (/**@type {bigint}*/ len) => {
-			if (len < 0)
-				for (let i = 0, n; i > len; i--, limNeg -= 2n) {
-					n = limNeg
+	const C = {
+		/**
+		search in a finite range, or up-to `Infinity`
+		@param {number|bigint} count how many odd ints to check
+		@return counter-example if found, or `undefined` if not found
+		*/
+		search: count => {
+			if (count != count)
+				throw new RangeError('expected `len` to not be `NaN`, but got `NaN`')
+
+			if (count < 0) //negative
+				for (; count++ < 0; limNeg -= 2n) {
+					let n = limNeg
 					do n = f(n); while (n < limNeg)
 					if (n == limNeg) return limNeg
 				}
-			else
-				for (let i = 0, n; i < len; i++, limPos += 2n) {
-					n = limPos
+			else //unsigned
+				for (; count-- > 0; limPos += 2n) {
+					let n = limPos
 					do n = f(n); while (n > limPos)
 					if (n == limPos) return limPos
 				}
 		},
-		test: (/**@type {bigint}*/ n) => {
+		/**
+		check if `n` doesn't converge (it's a counter-example)
+		@param {bigint} n int to test for convergence
+		@return `true` if `n` is a CC, `false` if regular
+		*/
+		check: n => {
+			//remember the original
 			const m = n = trim(n)
+
 			if (n < 0n) {
 				if (n >= limNeg) return false
 				do n = f(n); while (n < limNeg)
@@ -48,6 +69,7 @@ const Collatz = (() => {
 				if (n <= limPos) return false
 				do n = f(n); while (n > limPos)
 			}
+			//check if it's a cycle
 			return n == m
 		},
 		get limitPos() { return limPos },
@@ -63,5 +85,5 @@ const Collatz = (() => {
 			limNeg = x
 		}
 	}
-	return Collatz
+	return C
 })()
